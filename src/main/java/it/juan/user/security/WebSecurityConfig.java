416f13049632261500666e3bc3d2d4704c914d96
collantes21 <alvaritocollantes21@hotmail.com>
2024-02-01 14:09:39 +0100

    Acceso a Swagger

--- src/main/java/it/juan/user/security/WebSecurityConfig.java
+++ src/main/java/it/juan/user/security/WebSecurityConfig.java
@@ -7,16 +7,34 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
+
+
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+
+    private static final String[] AUTH_WHITELIST = {
+            // -- Swagger UI v2            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)            "/v3/api-docs/**",
+            "/swagger-ui/**" };           // other public endpoints of your API may be appended to this array    };
+
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.csrf().disable()
                     .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
                     .authorizeRequests()
                     .antMatchers(HttpMethod.POST, "/api/user").permitAll()
+                    .antMatchers(AUTH_WHITELIST).permitAll()
                     .anyRequest().authenticated();
         }
 
 }
+
+
